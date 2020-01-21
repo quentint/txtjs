b0bab813e5327eb62992034fcf9dd620df27903f
Jeremy Shipman <jeremy@burnbright.net>
2020-01-22 07:48:20 +1300

    Compile typescript and docs via npm scripts, rather than gulp. Re-introduced pathseg polyfill via npm.

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,32 @@
 var gulp = require( 'gulp' );
-var ts = require( 'gulp-tsc' );
 var connect = require( 'gulp-connect' );
 var shell = require( 'gulp-shell' );
 var uglify = require('gulp-uglify' );
 var rename = require( 'gulp-rename' );
-var typedoc = require("gulp-typedoc");
 
 // 1541 - Claude Garamond was commissioned to create fonts for King Francis I
 // of France and established himself as the first type designer.
 
-gulp.task( 'compile' , function(){
-    return gulp.src( [ './src/txt/build.d.ts' ] )
-        .pipe( ts( {
-            module: 'none',
-            target: 'ES5',
-            out: 'txt.js',
-            outDir: './dist',
-            emitError: true,
-            declaration: true,
-            removeComments: true
-        } ) )
-        .pipe( gulp.dest( './dist' ) );
-});
-
-gulp.task('copy-vendor-files', function() {
+gulp.task('copy-easel', function() {
     return gulp.src('./node_modules/easeljs/lib/easeljs.js')
         .pipe(gulp.dest('./dist'))
 });
 
-gulp.task( 'build' , [ 'compile', 'copy-vendor-files' ] , function() {
+gulp.task('copy-pathseg', function() {
+  return gulp.src('./node_modules/pathseg/pathseg.js')
+      .pipe(gulp.dest('./dist'))
+});
+
+gulp.task( 'copy-vendor-files' , [ 'copy-easel', 'copy-pathseg' ] );
+
+gulp.task( 'build' , [ 'compile', 'copy-vendor-files' ], function () {
    gulp.src( ['./dist/txt.js'] )
       .pipe( uglify() )
       .pipe( rename({ suffix: '.min' } ) )
       .pipe( gulp.dest( './dist/' ) )
 });
 
-gulp.task( 'server' , [ 'build' ] , function () {
+gulp.task( 'server', function () {
   connect.server({
     port: 1541,
     livereload: false
@@ -45,17 +36,5 @@ gulp.task( 'server' , [ 'build' ] , function () {
 gulp.task( 'browser' , [ 'server' ] , shell.task( [
     /^win/.test( require( 'os' ).platform() ) ? 'start http://localhost:1541/' : 'open http://localhost:1541/'
 ] ) );
-
-gulp.task("docs", function() {
-    return gulp
-        .src(["src/**/*.ts"])
-        .pipe(typedoc({
-            module: "commonjs",
-            target: "es5",
-            out: "docs/",
-            name: "txtjs"
-        }))
-    ;
-});
 
 gulp.task( 'default' , [ 'browser' ] );
