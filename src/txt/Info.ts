38a09271bab50c3335b877aba6fa7c0a42e93381
Jeremy Shipman <jeremy@burnbright.net>
2020-01-22 07:48:56 +1300

    Bump Info class version number to match package version

--- src/txt/Info.ts
+++ src/txt/Info.ts
@@ -1,6 +1,6 @@
 module txt {
     export class Info{
-        static VERSION:string = "0.8.5";
+        static VERSION:string = "0.9.5";
         static LICENSE:string = "BSD-2-Clause";
         static CONTACT:string = "ted@light.ly";
     }
