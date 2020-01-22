e0fd7f385b6e88c74de22de0b2f6687831e5a940
Jeremy Shipman <jeremy@burnbright.net>
2020-01-22 21:23:31 +1300

    Split out common helper functions from examples into separate file

--- examples/CharacterText/loadtest.js
+++ examples/CharacterText/loadtest.js
@@ -1,41 +1,12 @@
 var canvas;
 var stage;
 
-var PIXEL_RATIO = (function() {
-  var ctx = document.createElement("canvas").getContext("2d"),
-    dpr = window.devicePixelRatio || 1,
-    bsr =
-      ctx.webkitBackingStorePixelRatio ||
-      ctx.mozBackingStorePixelRatio ||
-      ctx.msBackingStorePixelRatio ||
-      ctx.oBackingStorePixelRatio ||
-      ctx.backingStorePixelRatio ||
-      1;
-  return dpr / bsr;
-})();
-
-createHiDPICanvas = function(w, h, ratio) {
-  if (!ratio) {
-    ratio = PIXEL_RATIO;
-  }
-  var can = document.createElement("canvas");
-  can.width = w * ratio;
-  can.height = h * ratio;
-  can.style.width = w + "px";
-  can.style.height = h + "px";
-  can.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
-  return can;
-};
-
 function init() {
   canvas = createHiDPICanvas(1000, 1000, 2);
-  //console.log( canvas );
   document.body.appendChild(canvas);
   stage = new createjs.Stage(canvas);
-  var style;
   var i = 1;
   var h = 8;
-  var text;
   while (i < 30) {
     h = h + 5 * i;
     stage.addChild(
--- examples/Text/tracking_layout_test.js
+++ examples/Text/tracking_layout_test.js
@@ -1,32 +1,6 @@
 var canvas;
 var stage;
 
-var PIXEL_RATIO = (function() {
-  var ctx = document.createElement("canvas").getContext("2d"),
-    dpr = window.devicePixelRatio || 1,
-    bsr =
-      ctx.webkitBackingStorePixelRatio ||
-      ctx.mozBackingStorePixelRatio ||
-      ctx.msBackingStorePixelRatio ||
-      ctx.oBackingStorePixelRatio ||
-      ctx.backingStorePixelRatio ||
-      1;
-  return dpr / bsr;
-})();
-
-createHiDPICanvas = function(w, h, ratio) {
-  if (!ratio) {
-    ratio = PIXEL_RATIO;
-  }
-  var can = document.createElement("canvas");
-  can.width = w * ratio;
-  can.height = h * ratio;
-  can.style.width = w + "px";
-  can.style.height = h + "px";
-  can.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
-  return can;
-};
-
 function init() {
   canvas = createHiDPICanvas(2000, 2000, 2);
   document.body.appendChild(canvas);
