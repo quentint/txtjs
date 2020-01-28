a3365af846fae802ddad4eabf61d0ef4acf56758
Jeremy Shipman <jeremy@burnbright.net>
2020-01-28 20:19:01 +1300

    Perform setup and teardown of canvas inside describe block of tests

--- tests/basic.js
+++ tests/basic.js
@@ -1,17 +1,18 @@
-afterEach(function() {
-  if (canvas) {
-    document.body.removeChild(canvas);
-    canvas = null;
-  }
-});
-
 describe("Smoke test", function() {
-  it("Can load txt.js library and check version", function() {
+  afterEach(function() {
+    if (canvas) {
+      document.body.removeChild(canvas);
+      canvas = null;
+    }
+  });
+
+  it("Loads txt.js library and checks version", function() {
     expect(txt).not.toBeNull();
     expect(txt.Info.VERSION).toEqual("0.9.5"); // remember to update all parts of codebase
   });
 
   it("Renders", function() {
     init();
+    expect(stage.children.length).toBeGreaterThan(0);
   });
 });
