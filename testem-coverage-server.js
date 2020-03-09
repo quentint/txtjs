c00f47b3f38fbcfaa14e0263e5c0b05291a6cc3f
Jeremy Shipman <jeremy@burnbright.net>
2020-03-09 20:36:03 +1300

    Fix missing tmp dir, and added missing npm package

--- testem-coverage-server.js
+++ testem-coverage-server.js
@@ -47,10 +47,7 @@ function shutdownCoverageServer(callback) {
   server.close();
 
   // generate report
-  shell.exec("mkdir -p tmp && npx nyc report --temp-dir=tmp", function(
-    code,
-    output
-  ) {
+  shell.exec("npx nyc report --temp-dir=tmp", function(code, output) {
     if (code) return callback(code, output);
 
     // check on generated report
