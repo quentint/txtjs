02e796e3fda6e678d65ab3cf4805f8db0d70105a
Quentin <quentin.t@gmail.com>
2020-05-20 15:54:06 +0200

    Fix default color, add setText and getMeasuredHeight methods

--- src/CjsTextProxy.ts
+++ src/CjsTextProxy.ts
@@ -1,22 +1,20 @@
 import Text from "./Text";
+import Line from "./Line";
 
 export default class CjsTextProxy extends Text {
-
-	// font             >    Already mapped
-	// lineHeight       >    Already mapped
-	// text             >    Already mapped
-
-	// color            >    fillColor
-	// lineWidth        >    width
-	// textAlign        >    align
-
-	// maxWidth         >    Not supported
-	// outline          >    Not supported
-	// textBaseline     >    Not supported
 	
-	// TODO: getMeasuredHeight
+	// TODO: maxWidth         >    Not supported?
+	// TODO: outline          >    Not supported?
+	// TODO: textBaseline     >    Not supported?
+	
+	// TODO: getMeasuredLineHeight()
+	// TODO: getMeasuredWidth()
+	// TODO: getMetrics()
 
 	constructor(text, font, color = null, moreProps: {}) {
+		if (color === null) {
+			color = "#000";
+		}
 		const fontProps = CjsTextProxy._processFontArgument(font);
 		const props = {text, font: fontProps.font, size: fontProps.size, fillColor: color};
 		Object.assign(props, moreProps);
@@ -54,6 +52,19 @@ export default class CjsTextProxy extends Text {
 
 	set textAlign(value: number) {
 		this.align = value;
+	}
+	
+	setText(s: string) {
+		this.text = s;
+		this.layout();
+	}
+	
+	getMeasuredHeight() {
+		let h = 0;
+		this.lines.forEach((line: Line) => {
+			h += line.measuredHeight;
+		});
+		return h;
 	}
 
 }
