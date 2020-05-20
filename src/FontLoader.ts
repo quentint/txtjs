3b65431d5a0709b8aa676b410bf3b851c19bfd54
Quentin <quentin.t@gmail.com>
2020-05-20 11:40:38 +0200

    Allow font text preloading by extracting parsing method

--- src/FontLoader.ts
+++ src/FontLoader.ts
@@ -143,104 +143,8 @@ export default class FontLoader {
         if (this.cacheResponseText) {
           lines = this.cacheResponseText.split("\n");
         }
-        const len = lines.length;
-        let i = 0;
-        let line: string[];
-        let glyph: Glyph;
-        let lineLen;
-        while (i < len) {
-          line = lines[i].split("|");
-          switch (line[0]) {
-            case "0":
-              //properties
-              if (
-                line[1] == "id" ||
-                line[1] == "panose" ||
-                line[1] == "family" ||
-                line[1] == "font-style" ||
-                line[1] == "font-stretch"
-              ) {
-                font[line[1]] = line[2];
-              } else {
-                font[line[1]] = parseInt(line[2]);
-              }
-              break;
 
-            case "1":
-              //glyphs
-
-              glyph = new Glyph();
-              glyph.offset = parseInt(line[2]) / font.units;
-              glyph.path = line[3];
-              font.glyphs[line[1]] = glyph;
-              break;
-
-            case "2":
-              //kerning
-              if (font.kerning[line[1]] == undefined) {
-                font.kerning[line[1]] = {};
-              }
-              if (font.glyphs[line[1]] == undefined) {
-                glyph = new Glyph();
-                glyph.offset = font.default / font.units;
-                glyph.path = "";
-                font.glyphs[line[1]] = glyph;
-              }
-              font.glyphs[line[1]].kerning[line[2]] =
-                parseInt(line[3]) / font.units;
-              font.kerning[line[1]][line[2]] = parseInt(line[3]) / font.units;
-              break;
-
-            case "3":
-              line.shift();
-              lineLen = line.length;
-              for (let j = 0; j < lineLen; j++) {
-                const path = line[j].split("");
-                const pathLength = path.length;
-                let target = font.ligatures;
-                for (let k = 0; k < pathLength; k++) {
-                  if (target[path[k]] == undefined) {
-                    target[path[k]] = {};
-                  }
-                  if (k == pathLength - 1) {
-                    target[path[k]].glyph = font.glyphs[line[j]];
-                  }
-
-                  target = target[path[k]];
-                }
-                //font.ligatures[ line[ j ] ] = font.glyphs[ line[j] ]
-              }
-              break;
-          }
-          i++;
-        }
-        //character cloning
-        //clone bullet into multiple areas
-        font.cloneGlyph(183, 8226);
-        font.cloneGlyph(8729, 8226);
-        font.cloneGlyph(12539, 8226);
-        font.cloneGlyph(9702, 8226);
-        font.cloneGlyph(9679, 8226);
-        font.cloneGlyph(9675, 8226);
-
-        //define font adjustment values for font.top, font.middle, font.bottom
-        if (font.top == undefined) {
-          font.top = 0;
-        }
-        if (font.middle == undefined) {
-          font.middle = 0;
-        }
-        if (font.bottom == undefined) {
-          font.bottom = 0;
-        }
-
-        //level the font metadata
-        const lLen = font.targets.length;
-        font.loaded = true;
-        for (let l = 0; l < lLen; ++l) {
-          FontLoader.check(font.targets[l]);
-        }
-        font.targets = [];
+        FontLoader.setFontPropsFromTextLines(font, lines);
       };
       //check if cached
       if (req.cacheFont == true) {
@@ -254,5 +158,114 @@ export default class FontLoader {
         req.send();
       }
     }
+  }
+  
+  static setFontPropsFromTextLines(font: Font, lines: string[]): Font {
+    const len = lines.length;
+    let i = 0;
+    let line: string[];
+    let glyph: Glyph;
+    let lineLen;
+    while (i < len) {
+      line = lines[i].split("|");
+      switch (line[0]) {
+        case "0":
+          //properties
+          if (
+              line[1] == "id" ||
+              line[1] == "panose" ||
+              line[1] == "family" ||
+              line[1] == "font-style" ||
+              line[1] == "font-stretch"
+          ) {
+            font[line[1]] = line[2];
+          } else {
+            font[line[1]] = parseInt(line[2]);
+          }
+          break;
+
+        case "1":
+          //glyphs
+
+          glyph = new Glyph();
+          glyph.offset = parseInt(line[2]) / font.units;
+          glyph.path = line[3];
+          font.glyphs[line[1]] = glyph;
+          break;
+
+        case "2":
+          //kerning
+          if (font.kerning[line[1]] == undefined) {
+            font.kerning[line[1]] = {};
+          }
+          if (font.glyphs[line[1]] == undefined) {
+            glyph = new Glyph();
+            glyph.offset = font.default / font.units;
+            glyph.path = "";
+            font.glyphs[line[1]] = glyph;
+          }
+          font.glyphs[line[1]].kerning[line[2]] =
+              parseInt(line[3]) / font.units;
+          font.kerning[line[1]][line[2]] = parseInt(line[3]) / font.units;
+          break;
+
+        case "3":
+          line.shift();
+          lineLen = line.length;
+          for (let j = 0; j < lineLen; j++) {
+            const path = line[j].split("");
+            const pathLength = path.length;
+            let target = font.ligatures;
+            for (let k = 0; k < pathLength; k++) {
+              if (target[path[k]] == undefined) {
+                target[path[k]] = {};
+              }
+              if (k == pathLength - 1) {
+                target[path[k]].glyph = font.glyphs[line[j]];
+              }
+
+              target = target[path[k]];
+            }
+            //font.ligatures[ line[ j ] ] = font.glyphs[ line[j] ]
+          }
+          break;
+      }
+      i++;
+    }
+    //character cloning
+    //clone bullet into multiple areas
+    font.cloneGlyph(183, 8226);
+    font.cloneGlyph(8729, 8226);
+    font.cloneGlyph(12539, 8226);
+    font.cloneGlyph(9702, 8226);
+    font.cloneGlyph(9679, 8226);
+    font.cloneGlyph(9675, 8226);
+
+    //define font adjustment values for font.top, font.middle, font.bottom
+    if (font.top == undefined) {
+      font.top = 0;
+    }
+    if (font.middle == undefined) {
+      font.middle = 0;
+    }
+    if (font.bottom == undefined) {
+      font.bottom = 0;
+    }
+
+    //level the font metadata
+    const lLen = font.targets.length;
+    font.loaded = true;
+    for (let l = 0; l < lLen; ++l) {
+      FontLoader.check(font.targets[l]);
+    }
+    font.targets = [];
+    
+    return font;
+  }
+  
+  static preloadFontFromText(fontName: string, text: string): Font {
+    const font: Font = (FontLoader.fonts[fontName] = new Font());
+    FontLoader.setFontPropsFromTextLines(font, text.split('\n'));
+    return font;
   }
 }
