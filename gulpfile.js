056fc59e5beaf72a36974f718a02cee8de485a51
Jeremy Shipman <jeremy@burnbright.net>
2020-06-15 21:28:54 +1200

    Use npm live-server as dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require("gulp");
-var connect = require("gulp-connect");
-var shell = require("gulp-shell");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 
@@ -15,21 +13,3 @@ gulp.task("build", ["compile"], function() {
     .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("server", function() {
-  connect.server({
-    port: 1541,
-    livereload: false
-  });
-});
-
-gulp.task(
-  "browser",
-  ["server"],
-  shell.task([
-    /^win/.test(require("os").platform())
-      ? "start http://localhost:1541/"
-      : "open http://localhost:1541/"
-  ])
-);
-
-gulp.task("default", ["browser"]);
