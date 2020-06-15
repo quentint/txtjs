6f3665a856a702e264bc05e38486ce52698eceb9
Jeremy Shipman <jeremy@burnbright.net>
2020-06-15 21:25:26 +1200

    Use npm copyfiles instead of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,7 @@ var rename = require("gulp-rename");
 // 1541 - Claude Garamond was commissioned to create fonts for King Francis I
 // of France and established himself as the first type designer.
 
-gulp.task("copy-easel", function() {
-  return gulp
-    .src("./node_modules/easeljs/lib/easeljs.js")
-    .pipe(gulp.dest("./dist"));
-});
-
-gulp.task("copy-pathseg", function() {
-  return gulp
-    .src("./node_modules/pathseg/pathseg.js")
-    .pipe(gulp.dest("./dist"));
-});
-
-gulp.task("copy-vendor-files", ["copy-easel", "copy-pathseg"]);
-
-gulp.task("build", ["compile", "copy-vendor-files"], function() {
+gulp.task("build", ["compile"], function() {
   gulp
     .src(["./dist/txt.js"])
     .pipe(uglify())
