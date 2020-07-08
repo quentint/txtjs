9139aa7356ed4b2d10655d1b25790ef94fc3b341
Jeremy Shipman <jeremy@burnbright.net>
2020-07-09 07:41:14 +1200

    Tidy rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
-// since rollup plugins cant be enabled from the command-line
+// since rollup plugins cant be enabled from the command-line,
+// we need to configure the sourcemaps plugin here
 export default {
-  plugins: [sourcemaps()],
+  plugins: [sourcemaps()]
 };
